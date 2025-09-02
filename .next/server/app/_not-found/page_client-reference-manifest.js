5936cc82f9ecc9699aedb03cf32347f985023a92
redefinesolutions <110117776+redefinesolutions@users.noreply.github.com>
2025-09-02 18:42:15 +0530

    Deleted routes-manifest.json

--- .next/server/app/_not-found/page_client-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-globalThis.__RSC_MANIFEST=(globalThis.__RSC_MANIFEST||{});globalThis.__RSC_MANIFEST["/_not-found/page"]={"moduleLoading":{"prefix":"/_next/"},"ssrModuleMapping":{},"edgeSSRModuleMapping":{},"clientModules":{},"entryCSSFiles":{"/home/project/":[],"/home/project/app/_not-found/page":[]},"rscModuleMapping":{},"edgeRscModuleMapping":{}}
