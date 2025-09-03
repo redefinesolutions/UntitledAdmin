f3b41496e84c4cda65815f7e116d38e6e3a1ea10
redefinesolutions <110117776+redefinesolutions@users.noreply.github.com>
2025-09-03 18:02:23 +0530

    Updated ProductsPage.stories.js

--- src/stories/ProductsPage.stories.js
+++ src/stories/ProductsPage.stories.js
@@ -1,14 +1,12 @@
-@@ .. @@
- export const Default = {
-   args: {}
--};
-+}
-+
-+export const EditMode = {
-+  args: {},
-+  play: async ({ canvasElement }) => {
-+    const canvas = within(canvasElement)
-+    const editButton = canvas.getByText('Edit Mode')
-+    await userEvent.click(editButton)
-+  }
-+}
+export const Default = {
+  args: {}
+};
+
+export const EditMode = {
+  args: {},
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+    const editButton = canvas.getByText('Edit Mode')
+    await userEvent.click(editButton)
+  }
+}
