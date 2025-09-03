a9a232663e46c5cd5af1e974fe22e77f6bb60b34
redefinesolutions <110117776+redefinesolutions@users.noreply.github.com>
2025-09-03 18:00:11 +0530

    Updated .gitignore

---
+++ .storybook/main.js
@@ -0,0 +1,19 @@
+
+
+/** @type { import('@storybook/vue3-vite').StorybookConfig } */
+const config = {
+  "stories": [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  "addons": [
+    "@chromatic-com/storybook",
+    "@storybook/addon-docs",
+    "@storybook/addon-onboarding"
+  ],
+  "framework": {
+    "name": "@storybook/vue3-vite",
+    "options": {}
+  }
+};
+export default config;
